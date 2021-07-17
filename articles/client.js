b5e2937023001187b1c97dbfc98302fb6aaf420b
Berry Karake <53060494+yeerkus@users.noreply.github.com>
2021-07-17 05:26:57 +0200

    Add files via upload

---
+++ articles/client.js
@@ -0,0 +1,13 @@
+const page = document.getElementById("page");
+// fetch("https://jsonplaceholder.typicode.com/users")
+//     .then(response => response.json())
+//     .then((data) => {
+//         data.forEach((article) => {
+//             page.innerHTML += `
+//                 <div class="card">
+//                     <h3>${article.name}</h3>
+//                     <p>${article.company.catchPhrase}</p>
+//                 </div>`
+//         })
+//     })
+//     .catch(err => console.log(err))
